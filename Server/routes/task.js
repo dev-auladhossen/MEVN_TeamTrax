4c5f0ca6d707cf176e023dcfe451a9f233228a2a
dev-auladhossen <auladhossen3021@gmail.com>
2025-06-19 08:38:32 +0600

    task details

--- Server/routes/task.js
+++ Server/routes/task.js
@@ -37,20 +37,46 @@ router.get("/tasks/project/:projectId", async (req, res) => {
 
 router.get("/task-details/:id", async (req, res) => {
   try {
-    const task = await Task.findById(req.params.id);
+    const task = await Task.findById(req.params.id).populate(
+      "projectId",
+      "name"
+    );
     if (!task) {
       return res.status(400).json({ message: "Task not found!" });
     }
     res.json(task);
+    console.log("from api", task);
   } catch (err) {
     console.error("Error fetching project by ID:", err.message);
     res.status(500).json({ message: "Server Error" });
   }
 });
 
-router.put("/tasks/:id", upload.array("attachments", 5), async (req, res) => {
+router.delete("/task/:id", async (req, res) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(req.params.id);
+    if (!deletedTask) {
+      return res.status(400).json({ message: "Task not found!" });
+    }
+    res.json({ message: "Task Deleted Successfully!" });
+  } catch (err) {
+    console.error("Delete Error:", err.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.put("/task/:id", upload.array("attachments", 5), async (req, res) => {
   const taskId = req.params.id;
-  const { title, description, status, assignedTo } = req.body;
+  const {
+    title,
+    description,
+    status,
+    priority,
+    projectId,
+    dueDate,
+    assignedTo,
+  } = req.body;
+  console.log("from api req.body", req.body);
   const attachments = req.files?.map((file) => file.filename) || [];
 
   try {
@@ -60,12 +86,14 @@ router.put("/tasks/:id", upload.array("attachments", 5), async (req, res) => {
         title,
         description,
         status,
+        priority,
+        projectId,
+        dueDate,
         assignedTo: JSON.parse(assignedTo),
         $push: { attachments: { $each: attachments } },
       },
       { new: true }
     );
-
     res.json(updatedTask);
   } catch (error) {
     console.error(error);
